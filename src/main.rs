611b207a802cd785ae4f514b520ff594b8027b07
Leo Kling <leo.kling.info@gmail.com>
2023-10-18 20:48:26 +0200

    final

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use std::arch::x86_64::CpuidResult;
 use std::net::IpAddr;
 use std::time::Duration;
 use std::{env, fs::File, io::BufReader};
 
 use dns_lookup::lookup_host;
 use dotenv::dotenv;
-use futures::StreamExt;
 use futures::future::join_all;
 use ipinfo::{IpInfo, IpInfoConfig};
+use plotpy::{Curve, Plot};
 use rand::random;
 use rayon::prelude::*;
 use surge_ping::{Client, Config, IcmpPacket, PingIdentifier, PingSequence, ICMP};
@@ -28,19 +27,23 @@ struct RecordWithIp {
 
 #[derive(Debug, serde::Deserialize, serde::Serialize, Clone)]
 struct RecordWithGeolocation {
-    // name: String,
-    // url: String,
     ip: String,
     location: String,
 }
 
 #[derive(Debug, serde::Deserialize, serde::Serialize, Clone)]
 struct RecordWithTime {
-    // name: String,
-    // url: String,
     ip: String,
     location: String,
     time: f64,
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize, Clone)]
+struct RecordWithDistance {
+    ip: String,
+    location: String,
+    time: f64,
+    distance: f64,
 }
 
 fn collect_ips() -> anyhow::Result<()> {
@@ -171,62 +174,108 @@ async fn ping(
     let payload = [0; 56];
     let mut pinger = client.pinger(addr, PingIdentifier(random())).await;
     pinger.timeout(Duration::from_secs(5));
-    // let mut interval = time::interval(Duration::from_secs(1));
-    // for idx in 0..5 {1
-    // interval.tick().await;
+
     let res = match pinger.ping(PingSequence(0), &payload).await {
-        Ok((IcmpPacket::V4(_), dur)) => {
-            // println!(
-            //     "No.{}: {} bytes from {}: icmp_seq={} ttl={:?} time={:0.2?}",
-            //     idx,
-            //     packet.get_size(),
-            //     packet.get_source(),
-            //     packet.get_sequence(),
-            //     packet.get_ttl(),
-            //     dur
-            // );
-            Some(RecordWithTime {
-                ip: record.ip.clone(),
-                location: record.location.clone(),
-                time: dur.as_secs_f64(),
-            })
-        }
-        Ok((IcmpPacket::V6(_), dur)) => {
-            // println!(
-            //     "No.{}: {} bytes from {}: icmp_seq={} hlim={} time={:0.2?}",
-            //     idx,
-            //     packet.get_size(),
-            //     packet.get_source(),
-            //     packet.get_sequence(),
-            //     packet.get_max_hop_limit(),
-            //     dur
-            // );
-            Some(RecordWithTime {
-                ip: record.ip.clone(),
-                location: record.location.clone(),
-                time: dur.as_secs_f64(),
-            })
-        }
+        Ok((IcmpPacket::V4(_), dur)) => Some(RecordWithTime {
+            ip: record.ip.clone(),
+            location: record.location.clone(),
+            time: dur.as_secs_f64(),
+        }),
+        Ok((IcmpPacket::V6(_), dur)) => Some(RecordWithTime {
+            ip: record.ip.clone(),
+            location: record.location.clone(),
+            time: dur.as_secs_f64(),
+        }),
         Err(e) => {
             println!("Err: {} ping {}", pinger.host, e);
             None
         }
     };
-    // }
-    // println!("[+] {} done.", pinger.host);
+
+    println!("[+] {} done.", pinger.host);
 
     res
 }
 
+async fn calculate_distances() -> anyhow::Result<()> {
+    let input = File::open("./with_times.json")?;
+    let records: Vec<RecordWithTime> = serde_json::from_reader(BufReader::new(input))?;
+
+    let mut cities = cities::all().to_vec();
+    cities.sort_by(|a, b| a.city.cmp(b.city));
+
+    let results = records
+        .iter()
+        .filter_map(|r| {
+            // binary search does not work because list is not sorted by city names
+            if let Ok(idx) = cities.binary_search_by(|c| c.city.cmp(r.location.as_str())) {
+                let city = &cities[idx];
+                let distance = geoutils::Location::new(city.latitude, city.longitude)
+                    .distance_to(&geoutils::Location::new(50.9375, 6.9603))
+                    .unwrap();
+                Some(RecordWithDistance {
+                    ip: r.ip.clone(),
+                    location: r.location.clone(),
+                    time: r.time,
+                    distance: distance.meters() / 1000f64,
+                })
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+
+    let output = File::create("./with_distances.json")?;
+    serde_json::to_writer_pretty(output, &results)?;
+
+    Ok(())
+}
+
+fn plot_data() -> anyhow::Result<()> {
+    let input = File::open("./with_distances.json")?;
+    let records: Vec<RecordWithDistance> = serde_json::from_reader(BufReader::new(input))?;
+
+    let mut curve = Curve::new();
+    curve.set_line_style("None");
+    curve.set_marker_style("o");
+    curve.set_marker_size(1.5);
+
+    curve.points_begin();
+    for r in records {
+        curve.points_add(r.time * 1000f64, r.distance);
+    }
+    curve.points_end();
+
+    let mut plot = Plot::new();
+    plot.add(&curve)
+        .grid_and_labels("Time (ms)", "Distance (km)")
+        .set_title("Ping time vs. distance")
+        .set_figure_size_points(1600., 1000.)
+        .set_ticks_x(100., 50., "");
+    plot.save("plot.png").unwrap();
+
+    let mut plot = Plot::new();
+    plot.add(&curve)
+        .grid_and_labels("Time (ms)", "Distance (km)")
+        .set_title("Ping time vs. distance")
+        .set_figure_size_points(1600., 1000.)
+        .set_log_x(true)
+        .set_log_y(true)
+        .set_ticks_x(0., 0., "");
+    plot.save("plot_log.png").unwrap();
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // dotenv()?;
-    //collect_ips()?;
+    dotenv()?;
+    // collect_ips()?;
     // collect_geolocations().await?;
-    ping_ips().await?;
-    // calculate distances
-    //https://openweathermap.org/api/geocoding-api#direct
-    //https://docs.rs/geoutils/latest/geoutils/
+    // ping_ips().await?;
+    // calculate_distances().await?;
+
+    plot_data()?;
 
     Ok(())
 }
